d1fc6dc5fb2279937ebe305d98ab6a62213ad8db
Seemant Kulleen <seemantk@gmail.com>
2022-01-19 16:06:07 -0800

    move the assemble() function into its own module

--- src/crepes/crepes.py
+++ src/crepes/crepes.py
@@ -5,54 +5,7 @@
 import os, boto3
 import argparse
 from cfn_tools import load_yaml, dump_yaml, dump_json
-import importify, jinjify 
-
-
-# Assemble all the components of a stack into a single cloudformation::stack object
-def assemble(stack, kwargs, imports={}):
-    # Create a dictionary to hold all the information
-    cfn_stack = { 'AWSTemplateFormatVersion': '2010-09-09' }
-
-    # Section names start with '0'
-    sections = [l for l in os.listdir(stack) if l.startswith('0')]
-    sections.sort()
-
-    for section in sections:
-        sec = section.split('_')[1]
-
-        # Create an empty dictionary key for this section for all the files to add their data
-        cfn_stack[sec] = {}
-
-        # Traverse the directory hierarchy, parsing all files along the way
-        for dirpath, dirs, files in os.walk(os.path.join(stack, section)):
-            for f in files:
-                # Process each file as a Jinja template first
-                contents = jinjify.process_template(os.path.join(dirpath, f), kwargs)
-                filename = f.lower()
-
-                if filename.endswith('.yml') or filename.endswith('yaml'):
-                    # Process YAML files (most everything)
-                    print("Flipping %s" % os.path.relpath(os.path.join(dirpath, f), '.'))
-
-                    # if this is an import template (rare)
-                    if sec == 'Resources' and imports:
-                        # Only import keys specified in the file in arg.imports
-                        keys = [key for key in contents.keys() if key in imports.keys()]
-                    else:
-                        keys = contents.keys()
-
-                    for key in keys:
-                        # Append each YAML object to the cfn_stack
-                        if contents[key]:
-                            cfn_stack[sec][key] = contents[key]
-                elif filename.endswith('.txt') or filename.endswith('text'):
-                    # Process the description text file
-                    cfn_stack[sec] = contents
-                else:
-                    # Bypass files with other content
-                    continue
-
-    return {k: v for k, v in cfn_stack.items() if v} # discard null/empty keys
+import importify, jinjify, stackify
 
 
 def parse_command_line_arguments():
@@ -107,7 +60,7 @@ def main():
 
     # Assemble the stack into a dict and convert that to YAML 
     # The imports_template variable is {} if not importing
-    stack = assemble(args.directory, kwargs, imports=imports_template)
+    stack = stackify.assemble(args.directory, kwargs, imports=imports_template)
 
     if args.imports:
         # Import template is not allowed to have an Outputs section
---
+++ src/crepes/stackify.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+# Copyright 2020-2022 Seemant Kulleen <seemantk@gmail.com>
+
+import os
+from cfn_tools import load_yaml, dump_yaml, dump_json
+import importify, jinjify
+
+
+# Assemble all the components of a stack into a single cloudformation::stack object
+def assemble(stack, kwargs, imports={}):
+    # Create a dictionary to hold all the information
+    cfn_stack = { 'AWSTemplateFormatVersion': '2010-09-09' }
+
+    # Section names start with '0'
+    sections = [l for l in os.listdir(stack) if l.startswith('0')]
+    sections.sort()
+
+    for section in sections:
+        sec = section.split('_')[1]
+
+        # Create an empty dictionary key for this section for all the files to add their data
+        cfn_stack[sec] = {}
+
+        # Traverse the directory hierarchy, parsing all files along the way
+        for dirpath, dirs, files in os.walk(os.path.join(stack, section)):
+            for f in files:
+                # Process each file as a Jinja template first
+                contents = jinjify.process_template(os.path.join(dirpath, f), kwargs)
+                filename = f.lower()
+
+                if filename.endswith('.yml') or filename.endswith('yaml'):
+                    # Process YAML files (most everything)
+                    print("Flipping %s" % os.path.relpath(os.path.join(dirpath, f), '.'))
+
+                    # if this is an import template (rare)
+                    if sec == 'Resources' and imports:
+                        # Only import keys specified in the imported resources file
+                        keys = [key for key in contents.keys() if key in imports.keys()]
+                    else:
+                        keys = contents.keys()
+
+                    for key in keys:
+                        # Append each YAML object to the cfn_stack
+                        if contents[key]:
+                            cfn_stack[sec][key] = contents[key]
+
+                elif filename.endswith('.txt') or filename.endswith('text'):
+                    # Process the description text file
+                    cfn_stack[sec] = contents
+                else:
+                    # Bypass files with other content
+                    continue
+
+    # If this is an import template, then it is not allowed to have an Outputs section
+    if imports: cfn_stack.pop('Outputs', None)
+
+    return {k: v for k, v in cfn_stack.items() if v} # discard null/empty keys
