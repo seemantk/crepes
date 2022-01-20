b972d24c14f37c9e17716d44e995e13dda4ca5b6
Seemant Kulleen <seemantk@gmail.com>
2022-01-19 19:14:03 -0800

    turn this into a module rather than a main script, which means no argparsing, just args

--- src/crepes/cluseau.py
+++ src/crepes/cluseau.py
@@ -11,12 +11,6 @@ import os, sys
 import argparse
 from cfn_tools import load_yaml, dump_json, dump_yaml
 
-# Parameters
-parser = argparse.ArgumentParser(description='breakdown a YAML CloudFormation template into component destination directory')
-parser.add_argument('destdir', metavar='destdir', type=str, default='', help='destination directory')
-parser.add_argument('--source', dest='template', type=str, help='source CloudFormation template')
-args = parser.parse_args()
-
 # Constants
 SECTIONS = ['Description', 'Metadata', 'Parameters', 'Mappings','Conditions', 'Transform', 'Resources', 'Outputs']
 
@@ -24,10 +18,10 @@ SECTIONS = ['Description', 'Metadata', 'Parameters', 'Mappings','Conditions', 'T
 #
 # Main Function
 #
-def main():
-    if args.template:
+def cluseau(template, destdir):
+    if template:
         # Read the introspected CloudFormer template
-        with open(args.template) as f:
+        with open(template) as f:
             current = load_yaml(f)
 
     # Create the section directories if they don't exist
@@ -36,30 +30,30 @@ def main():
         secdir = '_'.join([str(i).zfill(2), section])
 
         try:
-            os.makedirs(os.path.join(args.destdir, secdir), exist_ok=True)
+            os.makedirs(os.path.join(destdir, secdir), exist_ok=True)
 
             if(i == 1): # Write out the description file
-                if args.template: # Create a description placeholder
-                    process_description(secdir, current[section])
+                if template: # Create a description placeholder
+                    process_description(secdir, current[section], destdir)
                 else:
-                    process_description(secdir, 'Description of the stack goes here.')
+                    process_description(secdir, 'Description of the stack goes here.', destdir)
 
             elif(i == 7): # Process the AWS Resources
-                if args.template:
-                    process_resources(secdir, current[section])
+                if template:
+                    process_resources(secdir, current[section], destdir)
             else:
-                if args.template:
-                    process_section(secdir, section, current[section])
+                if template:
+                    process_section(secdir, section, current[section], destdir)
         except KeyError:
             continue
 
 # Helper Functions
-def process_description(secdir, description):
-    with open(os.path.join(args.destdir, secdir, 'description.txt'), 'w') as f:
+def process_description(secdir, description, destdir):
+    with open(os.path.join(destdir, secdir, 'description.txt'), 'w') as f:
         f.write(description)
 
 
-def process_resources(secdir, resources):
+def process_resources(secdir, resources, destdir):
     for resname in resources:
         # Grab the AWS resource type (e.g. AWS::EC2::LaunchInstance)
         resource = resources[resname]
@@ -68,7 +62,7 @@ def process_resources(secdir, resources):
         # Discard the 'AWS' part
         resource_type.pop()
         resource_type.reverse()
-        dirname = os.sep.join([args.destdir, secdir, os.sep.join(resource_type)])
+        dirname = os.sep.join([destdir, secdir, os.sep.join(resource_type)])
         filename = '.'.join([resname, 'yml'])
 
         # put each resource into a directory nmed for its type
@@ -83,9 +77,9 @@ def process_resources(secdir, resources):
 
 
 
-def process_section(secdir, section, content):
+def process_section(secdir, section, content, destdir):
     print("processing %s" % secdir)
-    with open(os.path.join(args.destdir, secdir, '%s.yml' % section.lower()), 'w') as f:
+    with open(os.path.join(destdir, secdir, '%s.yml' % section.lower()), 'w') as f:
         f.write(dump_yaml({'%s' % content}))
 
 
